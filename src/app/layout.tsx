5cac517371ab5793d5b613f1b00b96f6bcc60136
jongseok-lee <4145+jongseok-lee@users.noreply.git-dev.linecorp.com>
2023-07-26 00:50:05 +0900

    feat: Building the NavBar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,36 @@
-import '@/styles/globals.css'
+import NavBar from "@/components/NavBar";
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
 
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
-}
+  title: "Breadit",
+  description: "A Reddit clone built with Next.js and TypeScript.",
+};
+
+const inter = Inter({
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slace-90 antialiased line-through",
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <NavBar />
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          {children}
+        </div>
+      </body>
     </html>
-  )
+  );
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,23 @@
+import { Icons } from "@/components/Icons";
+import { buttonVariants } from "@/components/ui/Button";
+import Link from "next/link";
+
+const NavBar = () => {
+  return (
+    <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
+      <div className="container max-w-7xl nh-full mx-auto flex items-center justify-between gap-2">
+        {/* LOGO */}
+        <Link href="/" className="flex gap-2 items-center">
+          <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6" />
+          <p className="hidden text-zinc-700 text-sm font-medium md:block">
+            Breadit
+          </p>
+        </Link>
+        {/* TODO: Search bar */}
+        <Link href="/sign-in" className={buttonVariants()}>Sign In</Link>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
