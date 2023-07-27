117829d48635cf9a886e9071fc141f1fe0a728fc
jongseok-lee <4145+jongseok-lee@users.noreply.git-dev.linecorp.com>
2023-07-28 08:09:48 +0900

    feat: Creating a performance-optimized post detail page

--- src/hooks/use-custom-toast.tsx
+++ src/hooks/use-custom-toast.tsx
@@ -1,24 +1,23 @@
-import { buttonVariants } from "@/components/ui/Button";
-import { toast } from "@/hooks/use-toast";
-import Link from "next/link";
+import { buttonVariants } from '@/components/ui/Button'
+import { toast } from '@/hooks/use-toast'
+import Link from 'next/link'
 
 export const useCustomToast = () => {
   const loginToast = () => {
     const { dismiss } = toast({
-      title: "Login required.",
-      description: "You need to be logged in to do that.",
-      variant: "destructive",
+      title: 'Login required.',
+      description: 'You need to be logged in to do that.',
+      variant: 'destructive',
       action: (
         <Link
-          href="/sign-in"
+          href='/sign-in'
           onClick={() => dismiss()}
-          className={buttonVariants({ variant: "outline" })}
+          className={buttonVariants({ variant: 'outline' })}
         >
           Login
         </Link>
       ),
-    });
-  };
-
-  return { loginToast };
-};
+    })
+  }
+  return { loginToast }
+}
