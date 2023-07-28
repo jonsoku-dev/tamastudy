c69032b2edff20eb2396c2ba11cbb0c0f3f58ad3
jongseok-lee <4145+jongseok-lee@users.noreply.git-dev.linecorp.com>
2023-07-28 10:41:30 +0900

    feat: replying to comments

--- src/components/CommentsSection.tsx
+++ src/components/CommentsSection.tsx
@@ -1,6 +1,6 @@
 import { getAuthSession } from '@/lib/auth'
 import { db } from '@/lib/db'
-import PostComment from '@/components/PostComment'
+import PostComment from '@/components/comments/PostComment'
 import CreateComment from '@/components/CreateComment'
 
 interface CommentsSectionProps {
@@ -47,7 +47,42 @@ const CommentsSection = async ({ postId }: CommentsSectionProps) => {
 
             return (
               <div key={topLevelComment.id} className='flex flex-col'>
-                <PostComment comment={topLevelComment} />
+                <div className='mb-2'>
+                  <PostComment
+                    comment={topLevelComment}
+                    postId={postId}
+                    currentVote={topLevelCommentVote}
+                    votesAmt={topLevelCommentVotesAmt}
+                  />
+                </div>
+                {/* Render replies */}
+                {topLevelComment.replies
+                  .sort((a, b) => b.votes.length - a.votes.length) // Sort replies by most liked
+                  .map((reply) => {
+                    const replyVotesAmt = reply.votes.reduce((acc, vote) => {
+                      if (vote.type === 'UP') return acc + 1
+                      if (vote.type === 'DOWN') return acc - 1
+                      return acc
+                    }, 0)
+
+                    const replyVote = reply.votes.find(
+                      (vote) => vote.userId === session?.user.id,
+                    )
+
+                    return (
+                      <div
+                        key={reply.id}
+                        className='ml-2 py-2 pl-4 border-l-2 border-zinc-200'
+                      >
+                        <PostComment
+                          comment={reply}
+                          currentVote={replyVote}
+                          votesAmt={replyVotesAmt}
+                          postId={postId}
+                        />
+                      </div>
+                    )
+                  })}
               </div>
             )
           })}
