81ac38b92eef1256c6fb84916e1c808b4d925e03
jongseok-lee <4145+jongseok-lee@users.noreply.git-dev.linecorp.com>
2023-07-28 11:13:45 +0900

    feat: letting users change their username

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,20 +1,20 @@
-import EditorOutput from "@/components/EditorOutput";
-import PostVoteClient from "@/components/post-vote/PostVoteClient";
-import { formatTimeToNow } from "@/lib/utils";
-import { Post, User, Vote } from "@prisma/client";
-import { MessageSquare } from "lucide-react";
-import { FC, useRef } from "react";
+import EditorOutput from '@/components/EditorOutput'
+import PostVoteClient from '@/components/post-vote/PostVoteClient'
+import { formatTimeToNow } from '@/lib/utils'
+import { Post, User, Vote } from '@prisma/client'
+import { MessageSquare } from 'lucide-react'
+import { FC, useRef } from 'react'
 
-type PartialVote = Pick<Vote, "type">;
+type PartialVote = Pick<Vote, 'type'>
 interface PostProps {
-  subredditName?: string;
+  subredditName?: string
   post: Post & {
-    author: User;
-    votes: Vote[];
-  };
-  commentAmt: number;
-  votesAmt: number;
-  currentVote?: PartialVote;
+    author: User
+    votes: Vote[]
+  }
+  commentAmt: number
+  votesAmt: number
+  currentVote?: PartialVote
 }
 
 const Post: FC<PostProps> = ({
@@ -24,61 +24,58 @@ const Post: FC<PostProps> = ({
   votesAmt: _votesAmt,
   currentVote,
 }) => {
-  const pRef = useRef<HTMLDivElement>(null);
+  const pRef = useRef<HTMLDivElement>(null)
 
   return (
-    <div className="rounded-md bh-white shadow">
-      <div className="px-6 py-4 flex justify-between">
+    <div className='rounded-md bh-white shadow'>
+      <div className='px-6 py-4 flex justify-between'>
         <PostVoteClient
           postId={post.id}
           initialVotesAmt={_votesAmt}
           initialVote={currentVote?.type}
         />
-        <div className="w-0 flex-1">
-          <div className="max-h-40 mt-1 text-xs text-gray-500">
+        <div className='w-0 flex-1'>
+          <div className='max-h-40 mt-1 text-xs text-gray-500'>
             {subredditName ? (
               <>
                 <a
-                  className="underline underline-offset-2 text-zinc-900 text-sm "
+                  className='underline underline-offset-2 text-zinc-900 text-sm '
                   href={`/r/${subredditName}`}
                 >
                   r/{subredditName}
                 </a>
-                <span className="px-1">•</span>
+                <span className='px-1'>•</span>
               </>
             ) : null}
-            <span>Posted by u/{post.author.name}</span>
+            <span>Posted by u/{post.author.username}</span>
             {formatTimeToNow(new Date(post.createdAt))}
           </div>
 
           <a href={`/r/${subredditName}/post/${post.id}`}>
-            <h1 className="text-lg font-semibold py-2 leading-6 text-gray-900">
+            <h1 className='text-lg font-semibold py-2 leading-6 text-gray-900'>
               {post.title}
             </h1>
           </a>
 
-          <div
-            ref={pRef}
-            className="relative text-sm max-h-40 w-full overflow-hidden"
-          >
+          <div ref={pRef} className='relative text-sm max-h-40 w-full overflow-hidden'>
             <EditorOutput content={post.content} />
 
             {pRef.current?.clientHeight === 160 ? (
-              <div className="absolute bottom-0 left-0 h-24 w-full bg-gradient-to-t from-white to-transparent"></div>
+              <div className='absolute bottom-0 left-0 h-24 w-full bg-gradient-to-t from-white to-transparent'></div>
             ) : null}
           </div>
         </div>
       </div>
-      <div className="bg-gray-50 z-20 text-sm p-4 sm:px-6">
+      <div className='bg-gray-50 z-20 text-sm p-4 sm:px-6'>
         <a
           href={`/r/${subredditName}/post/${post.id}`}
-          className="w-fit flex items-center gap-2"
+          className='w-fit flex items-center gap-2'
         >
-          <MessageSquare className="h-4 w-4" /> {commentAmt} comments
+          <MessageSquare className='h-4 w-4' /> {commentAmt} comments
         </a>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Post;
+export default Post
