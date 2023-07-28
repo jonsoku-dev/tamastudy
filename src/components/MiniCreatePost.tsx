e4a334683f062465cb99d2e2390dcd5e9b68e1ce
jongseok-lee <4145+jongseok-lee@users.noreply.git-dev.linecorp.com>
2023-07-28 11:17:03 +0900

    feat: finish

--- src/components/MiniCreatePost.tsx
+++ src/components/MiniCreatePost.tsx
@@ -1,54 +1,48 @@
-"use client";
+'use client'
 
-import UserAvatar from "@/components/UserAvatar";
-import { Button } from "@/components/ui/Button";
-import { Input } from "@/components/ui/Input";
-import { ImageIcon, Link2 } from "lucide-react";
-import { Session } from "next-auth";
-import { usePathname, useRouter } from "next/navigation";
-import { FC } from "react";
+import { Button } from '@/components/ui/Button'
+import { Input } from '@/components/ui/Input'
+import { Image as ImageIcon, Link2 } from 'lucide-react'
+import { FC } from 'react'
+import type { Session } from 'next-auth'
+import { usePathname, useRouter } from 'next/navigation'
+import UserAvatar from '@/components/UserAvatar'
 
 interface MiniCreatePostProps {
-  session: Session | null;
+  session: Session | null
 }
 
 const MiniCreatePost: FC<MiniCreatePostProps> = ({ session }) => {
-  const router = useRouter();
-  const pathname = usePathname();
+  const router = useRouter()
+  const pathname = usePathname()
 
   return (
-    <li className="overflow-hidden rounded-md bg-white shadow">
-      <div className="h-full px-6 py-4 sm:flex sm:justify-between gap-6">
-        <div className="relative">
+    <li className='overflow-hidden rounded-md bg-white shadow'>
+      <div className='h-full px-6 py-4 flex justify-between gap-6'>
+        <div className='relative'>
           <UserAvatar
             user={{
               name: session?.user.name || null,
               image: session?.user.image || null,
             }}
           />
-          <span className="absolute bottom-0 right-0 rounded-full w-3 h-3 bg-green-500 outline outline-2 outline-white"></span>
-        </div>
 
+          <span className='absolute bottom-0 right-0 rounded-full w-3 h-3 bg-green-500 outline outline-2 outline-white' />
+        </div>
         <Input
+          onClick={() => router.push(pathname + '/submit')}
           readOnly
-          onClick={() => router.push(pathname + "/submit")}
-          placeholder="Create Post"
+          placeholder='Create post'
         />
-        <Button
-          variant="ghost"
-          onClick={() => router.push(pathname + "/submit")}
-        >
-          <ImageIcon className="text-zinc-600" />
+        <Button onClick={() => router.push(pathname + '/submit')} variant='ghost'>
+          <ImageIcon className='text-zinc-600' />
         </Button>
-        <Button
-          variant="ghost"
-          onClick={() => router.push(pathname + "/submit")}
-        >
-          <Link2 className="text-zinc-600" />
+        <Button onClick={() => router.push(pathname + '/submit')} variant='ghost'>
+          <Link2 className='text-zinc-600' />
         </Button>
       </div>
     </li>
-  );
-};
+  )
+}
 
-export default MiniCreatePost;
+export default MiniCreatePost
