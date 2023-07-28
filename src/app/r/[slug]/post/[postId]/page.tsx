8995bf03b191ab9bad541bf3764ac9d3a9bdcb10
jongseok-lee <4145+jongseok-lee@users.noreply.git-dev.linecorp.com>
2023-07-28 10:20:30 +0900

    feat: Creating the comments section

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -1,30 +1,29 @@
-import EditorOutput from "@/components/EditorOutput";
-import PostVoteServer from "@/components/post-vote/PostVoteServer";
-import { buttonVariants } from "@/components/ui/Button";
-import { db } from "@/lib/db";
-import { redis } from "@/lib/redis";
-import { formatTimeToNow } from "@/lib/utils";
-import { CachedPost } from "@/types/redis";
-import { Post, User, Vote } from "@prisma/client";
-import { ArrowBigDown, ArrowBigUp, Loader2 } from "lucide-react";
-import { notFound } from "next/navigation";
-import { Suspense } from "react";
+import EditorOutput from '@/components/EditorOutput'
+import PostVoteServer from '@/components/post-vote/PostVoteServer'
+import { buttonVariants } from '@/components/ui/Button'
+import { db } from '@/lib/db'
+import { redis } from '@/lib/redis'
+import { formatTimeToNow } from '@/lib/utils'
+import { CachedPost } from '@/types/redis'
+import { Post, User, Vote } from '@prisma/client'
+import { ArrowBigDown, ArrowBigUp, Loader2 } from 'lucide-react'
+import { notFound } from 'next/navigation'
+import { Suspense } from 'react'
+import CommentsSection from '@/components/CommentsSection'
 
 interface PageProps {
   params: {
-    postId: string;
-  };
+    postId: string
+  }
 }
 
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
+export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
 
 const Page = async ({ params }: PageProps) => {
-  const cachedPost = (await redis.hgetall(
-    `post:${params.postId}`
-  )) as CachedPost;
+  const cachedPost = (await redis.hgetall(`post:${params.postId}`)) as CachedPost
 
-  let post: (Post & { votes: Vote[]; author: User }) | null = null;
+  let post: (Post & { votes: Vote[]; author: User }) | null = null
 
   if (!cachedPost) {
     post = await db.post.findFirst({
@@ -35,14 +34,14 @@ const Page = async ({ params }: PageProps) => {
         votes: true,
         author: true,
       },
-    });
+    })
   }
 
-  if (!post && !cachedPost) return notFound();
+  if (!post && !cachedPost) return notFound()
 
   return (
     <div>
-      <div className="h-full flex flex-col sm:flex-row items-center sm:items-start justify-between">
+      <div className='h-full flex flex-col sm:flex-row items-center sm:items-start justify-between'>
         <Suspense fallback={<PostVoteShell />}>
           {/* @ts-expect-error server component */}
           <PostVoteServer
@@ -55,47 +54,52 @@ const Page = async ({ params }: PageProps) => {
                 include: {
                   votes: true,
                 },
-              });
+              })
             }}
           />
         </Suspense>
 
-        <div className="sm:w-0 w-full flex-1 bg-white p-4 rounded-sm">
-          <p className="max-h-40 mt-1 truncate text-xs text-gray-500">
-            Posted by u/{post?.author.name ?? cachedPost.authorUsername}{" "}
+        <div className='sm:w-0 w-full flex-1 bg-white p-4 rounded-sm'>
+          <p className='max-h-40 mt-1 truncate text-xs text-gray-500'>
+            Posted by u/{post?.author.name ?? cachedPost.authorUsername}{' '}
             {formatTimeToNow(new Date(post?.createdAt ?? cachedPost.createdAt))}
           </p>
 
-          <h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">
+          <h1 className='text-xl font-semibold py-2 leading-6 text-gray-900'>
             {post?.title ?? cachedPost.title}
           </h1>
 
           <EditorOutput content={post?.content ?? cachedPost.content} />
+
+          <Suspense fallback={<Loader2 className='h-5 w-5 animate-spin text-zinc-500' />}>
+            {/* @ts-expect-error server component */}
+            <CommentsSection postId={post.id} />
+          </Suspense>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 function PostVoteShell() {
   return (
-    <div className="flex items-center flex-col pr-6 w-20">
+    <div className='flex items-center flex-col pr-6 w-20'>
       {/* upvote */}
-      <div className={buttonVariants({ variant: "ghost" })}>
-        <ArrowBigUp className="h-5 w-5 text-zinc-700" />
+      <div className={buttonVariants({ variant: 'ghost' })}>
+        <ArrowBigUp className='h-5 w-5 text-zinc-700' />
       </div>
 
       {/* score */}
-      <div className="text-center py-2 font-medium text-sm text-zinc-900">
-        <Loader2 className="h-3 w-3 animate-spin" />
+      <div className='text-center py-2 font-medium text-sm text-zinc-900'>
+        <Loader2 className='h-3 w-3 animate-spin' />
       </div>
 
       {/* downvote */}
-      <div className={buttonVariants({ variant: "ghost" })}>
-        <ArrowBigDown className="h-5 w-5 text-zinc-700" />
+      <div className={buttonVariants({ variant: 'ghost' })}>
+        <ArrowBigDown className='h-5 w-5 text-zinc-700' />
       </div>
     </div>
-  );
+  )
 }
 
-export default Page;
+export default Page
