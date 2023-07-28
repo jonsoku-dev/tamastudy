a22237bb13292294927ae1337650452111fab2f2
jongseok-lee <4145+jongseok-lee@users.noreply.git-dev.linecorp.com>
2023-07-28 11:07:01 +0900

    feat: creating searchbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,32 +1,31 @@
-import { Icons } from "@/components/Icons";
-import UserAccountNav from "@/components/UserAccountNav";
-import { buttonVariants } from "@/components/ui/Button";
-import { getAuthSession } from "@/lib/auth";
-import Link from "next/link";
+import { Icons } from '@/components/Icons'
+import UserAccountNav from '@/components/UserAccountNav'
+import { buttonVariants } from '@/components/ui/Button'
+import { getAuthSession } from '@/lib/auth'
+import Link from 'next/link'
+import SearchBar from '@/components/SearchBar'
 
 const NavBar = async () => {
-  const session = await getAuthSession();
+  const session = await getAuthSession()
   return (
-    <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
-      <div className="container max-w-7xl nh-full mx-auto flex items-center justify-between gap-2">
+    <div className='fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2'>
+      <div className='container max-w-7xl nh-full mx-auto flex items-center justify-between gap-2'>
         {/* LOGO */}
-        <Link href="/" className="flex gap-2 items-center">
-          <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6" />
-          <p className="hidden text-zinc-700 text-sm font-medium md:block">
-            Breadit
-          </p>
+        <Link href='/' className='flex gap-2 items-center'>
+          <Icons.logo className='h-8 w-8 sm:h-6 sm:w-6' />
+          <p className='hidden text-zinc-700 text-sm font-medium md:block'>Breadit</p>
         </Link>
-        {/* TODO: Search bar */}
+        <SearchBar />
         {session ? (
           <UserAccountNav user={session.user} />
         ) : (
-          <Link href="/sign-in" className={buttonVariants()}>
+          <Link href='/sign-in' className={buttonVariants()}>
             Sign In
           </Link>
         )}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar
